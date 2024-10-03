8e3cf470a208ede56f9796483eb96ab284eaff59
xaviervasquez21 <xavier.vasquez@ucuenca.edu.ec>
2024-10-03 16:34:34 -0500

    Configuracion para Gorra

--- sketch.js
+++ sketch.js
@@ -1,68 +1,78 @@
-  // Classifier Variable
-  let classifier;
-  // Model URL
-  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/1UnhNVxQV/';
-  
-  // Video
-  let video;
-  let flippedVideo;
-  // To store the classification
-  let label = "";
-  let confiaza = 0;
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = "https://teachablemachine.withgoogle.com/models/1UnhNVxQV/";
 
-  // Load the model first
-  function preload() {
-    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-  }
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+let confiaza = 0;
 
-  function setup() {
-    createCanvas(320, 260);
-    // Create the video
-    video = createCapture(VIDEO);
-    video.size(320, 240);
-    video.hide();
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + "model.json");
+}
 
-    //flippedVideo = ml5.flipImage(video);
-    // Start classifying
-    classifyVideo();
-  }
+function setup() {
+  createCanvas(320, 260);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
 
-  function draw() {
-    background(0);
-    // Draw the video
-    image(video, 0, 0);
+  //flippedVideo = ml5.flipImage(video);
+  // Start classifying
+  classifyVideo();
+}
 
-    // Draw the label
+function draw() {
+  background(0);
+  // Draw the video
+  image(video, 0, 0);
+
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+
+  textSize(8);
+  textAlign(LEFT);
+  text(confiaza, 10, height - 4);
+
+  // Condicional para cambiar el fondo a turquesa
+  if (label == "Gorra" && confiaza > 0.9) {
+    filter(INVERT);
+    filter(GRAY);
+
     fill(255);
-    textSize(16);
+    textSize(80);
     textAlign(CENTER);
-    text(label, width / 2, height - 4);
+    text(label, width / 2, height / 2);
+  }
+}
 
-    textSize(8);
-    textAlign(LEFT);
-    text(confiaza, 10, height -4);
+// Get a prediction for the current video frame
+function classifyVideo() {
+  //flippedVideo = ml5.flipImage(video)
+  classifier.classify(video, gotResult);
+  //flippedVideo.remove();
+}
+
+// When we get a result
+function gotResult(results, error) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
   }
 
-  // Get a prediction for the current video frame
-  function classifyVideo() {
-    //flippedVideo = ml5.flipImage(video)
-    classifier.classify(video, gotResult);
-    //flippedVideo.remove();
-
-  }
-
-  // When we get a result
-  function gotResult(results, error) {
-    // If there is an error
-    if (error) {
-      console.error(error);
-      return;
-    }
-
-    // The results are in an array ordered by confidence.
-    // console.log(results[0]);
-    label = results[0].label;
-    confiaza = results[0].confidence;
-    // Classifiy again!
-    classifyVideo();
-  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  confiaza = results[0].confidence;
+  // Classifiy again!
+  classifyVideo();
+}
